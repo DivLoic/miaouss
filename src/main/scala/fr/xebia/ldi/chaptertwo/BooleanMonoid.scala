f03aec212825bf21c0c1bff46e0d09930b7739c0
Loïc <ldivad@isep.fr>
2018-03-05 23:29:43 +0100

    add the exe B1 Boolean Monoid

---
+++ src/main/scala/fr/xebia/ldi/chaptertwo/BooleanMonoid.scala
@@ -0,0 +1,43 @@
+package fr.xebia.ldi.chaptertwo
+
+/**
+  * Created by loicmdivad.
+  */
+object BoobleanMonoid {
+
+  trait Semigroup[A] {
+    def combine(x: A, y: A): A
+  }
+
+  trait Monoid[A] extends Semigroup[A] {
+    def empty: A
+  }
+
+  object Monoid {
+    def apply[A](implicit monoid: Monoid[A]) =
+      monoid
+  }
+
+  implicit val and: Monoid[Boolean] = new Monoid[Boolean] {
+    def empty: Boolean = true
+    def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  implicit val or: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+
+  implicit val either: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean =
+      (x && !y) || (!x && y)
+  }
+
+  implicit val xnor: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean =
+      (x || !y) && (!x || y)
+  }
+}
+
